1861e1d4ecfdd0415ec85c0182c66af7fea47c2c
SyFlores <syestevenflores@gmail.com>
2021-06-12 14:18:37 -0700

    The data is being read properly and inputted into the html.

--- static/js/UFO-level-1/app.js
+++ static/js/UFO-level-1/app.js
@@ -1,4 +1,16 @@
-from data.js
+// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Create a reference to tbody so we can create a row for every object in the array
+var bodySelect = d3.select("tbody")
+
+// We'll need to loop through each object in the array
+tableData.forEach(ufoData => {
+    var rowCreate = bodySelect.append("tr");
+    
+    // For each object, we'll need loop through and create & write a td tag
+    Object.entries(ufoData).forEach(([key, value]) => {
+        var cellCreate = rowCreate.append("td");
+        cellCreate.text(value)
+      });
+});
